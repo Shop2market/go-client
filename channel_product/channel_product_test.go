fb7c5dccee06627b4039d7c9a5478e5f38a1fb43
Jatin Naik <jsnaik@gmail.com>
2015-05-26 17:55:18 +0200

    Refactor, removed custom mock http server, replaced with ghttp

--- channel_product/channel_product_test.go
+++ channel_product/channel_product_test.go
@@ -4,13 +4,12 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
-	"net/url"
 
 	. "github.com/Shop2market/go-client/channel_product"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
 )
 
 var _ = Describe("Store", func() {
@@ -42,20 +41,28 @@ var _ = Describe("Store", func() {
 
 	Context("requests channel products", func() {
 		It("sends correct parameters", func() {
-			server := NewMockedServer("fixtures/channel_product_response.json")
-			Endpoint = server.URL
+			server := ghttp.NewServer()
+			server.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("GET", "/shops/1/publishers/5/products", "enabled=true&limit=10&skip=0"),
+					ghttp.RespondWith(http.StatusOK, "[]"),
+				),
+			)
+			Endpoint = server.URL()
 
 			Find(1, 5, 0, 10)
-
-			Expect(server.Requests).To(HaveLen(1))
-
-			marmosetUrl, _ := url.Parse("/shops/1/publishers/5/products?enabled=true&limit=10&skip=0")
-			Expect(server.Requests[0].URL).To(Equal(marmosetUrl))
 		})
 
 		It("deserializes response", func() {
-			server := NewMockedServer("fixtures/channel_product_response.json")
-			Endpoint = server.URL
+			content, err := ioutil.ReadFile("fixtures/channel_product_response.json")
+			Expect(err).NotTo(HaveOccurred())
+
+			server := ghttp.NewServer()
+			server.AppendHandlers(
+				ghttp.RespondWith(http.StatusOK, string(content)),
+			)
+
+			Endpoint = server.URL()
 
 			Expect(Find(1, 5, 0, 10)).To(Equal([]*Product{
 				&Product{
@@ -80,23 +87,3 @@ var _ = Describe("Store", func() {
 	})
 
 })
-
-type MockedServer struct {
-	*httptest.Server
-	Requests []*http.Request
-	Response []byte
-}
-
-func NewMockedServer(responseFileName string) *MockedServer {
-	ser := &MockedServer{}
-	ser.Server = httptest.NewServer(ser)
-	ser.Requests = []*http.Request{}
-	data, _ := ioutil.ReadFile(responseFileName)
-	ser.Response = data
-	return ser
-}
-func (ser *MockedServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	ser.Requests = append(ser.Requests, req)
-	resp.WriteHeader(200)
-	resp.Write(ser.Response)
-}
