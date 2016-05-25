1d6bbcb69da59cd8b04125d9ae73c4e0717681bb
Cremz <cremzinc@gmail.com>
2016-05-25 10:25:28 +0200

    PD-2862: added taxonomies api.

---
+++ shop/publisher/taxonomy/taxonomy.go
@@ -0,0 +1,95 @@
+package taxonomy
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+var Endpoint string
+var Username string
+var Password string
+
+type Query struct {
+	ShopId      int
+	PublisherId int
+}
+
+type Category struct {
+	Name       string   `json:"name"`
+	ParentID   int      `json:"parent_id"`
+	ExternalID string   `json:"external_id"`
+	Path       string   `json:"-"`
+	ID         int      `json:"id"`
+	CPC        *float64 `json:"cpc"`
+}
+type Taxonomy struct {
+	ID         int        `json:"id"`
+	Name       string     `json:"name"`
+	Categories []Category `json:"categories"`
+}
+
+// Finder - Main find functor, can be overloaded for stubs or assigned with package Find function
+type Finder func(query *Query) ([]Taxonomy, error)
+
+// Need to solve the problem of having cyclic items and categories with broken paths.
+func buildPaths(categories []Category) {
+	cats := categories
+	for i := range cats {
+		paths := buildPath(cats[i], cats)
+		cats[i].Path = strings.Join(paths, " -> ")
+	}
+}
+
+func buildPath(category Category, categories []Category) []string {
+	if category.ParentID == 0 {
+		return []string{category.Name}
+	}
+	for i := range categories {
+		if categories[i].ID == category.ParentID {
+			if category.ParentID > category.ID {
+				return []string{category.Name}
+			}
+			return append(buildPath(categories[i], categories), category.Name)
+		}
+	}
+	// at the moment we return the category name if the parent is missing
+	return []string{category.Name}
+}
+
+func Find(query *Query) ([]Taxonomy, error) {
+	url, err := apiUrl(query)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.SetBasicAuth(Username, Password)
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	taxonomy := []Taxonomy{}
+
+	if err := json.NewDecoder(response.Body).Decode(&taxonomy); err != nil {
+		return nil, err
+	}
+	for i := range taxonomy {
+		buildPaths(taxonomy[i].Categories)
+	}
+	return taxonomy, nil
+}
+
+func apiUrl(query *Query) (string, error) {
+	url, err := url.Parse(fmt.Sprintf("%s/api/v1/shops/%d/publishers/%d/publisher_taxonomies.json", Endpoint, query.ShopId, query.PublisherId))
+	if err != nil {
+		return "", err
+	}
+	return url.String(), nil
+}
