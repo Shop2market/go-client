ee2ed79f215522f17ed1f9d2d4a106d8b3663c9f
Stanislav O. Pogrebnyak <stanislav.pogrebnyak@gmail.com>
2016-11-21 14:56:48 +0100

    [+] Add logger for bonobo catalog

--- shop/product/product.go
+++ shop/product/product.go
@@ -163,10 +163,13 @@ func FindAll(shopId int) (<-chan ShopProduct, <-chan error) {
 		errorChannel <- err
 		return shopProductChannel, errorChannel
 	}
+	fmt.Printf("Opened stream for shop %d\n", shopId)
 	go func() {
 		defer close(shopProductChannel)
 		defer close(errorChannel)
 		defer resp.Body.Close()
+		defer fmt.Printf("Closed stream for shop %d\n", shopId)
+
 		decoder := json.NewDecoder(resp.Body)
 		var product BonoboProduct
 		var shopProduct ShopProduct
